11fb685b21b0da924541983b49eab4472e6bc3fa
Edem Tsalah <edem.tsalah@gmail.com>
2023-02-07 22:59:03 +0000

    started saving scraped data into models

--- models/member.js
+++ models/member.js
@@ -1,14 +1,22 @@
 class Member {
 
-  constructor(member_id, name, party, partAbbr, constituency, region, photo, profile) {
+  constructor(member_id, name, party, constituency, region, photo, profile) {
     this.member_id = member_id;
     this.name = name;
     this.party = party;
-    this.partAbbr = partyAbbr;
     this.constituency = constituency;
     this.region = region;
     this.photo = photo;
-    this.profile = profile; 
+    this.profile = profile;
   }
 
+  toJson () {
+    return {
+      'member_id': this.member_id, 'name': this.name, 'party': this.party,
+      'constituency': this.constituency, 'region': this.region,
+      'photo': this.photo, 'profile': this.profile
+    };
+  }
 }
+
+export default Member;
