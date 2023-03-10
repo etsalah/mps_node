2cd9c5fd4479a7d49f08e8d1c3b4886e33a85b76
Edem Tsalah <edem.tsalah@gamil.com>
2023-03-10 00:36:32 +0000

    added script to run the scrapping code periodically in background so that api endpoint just gets the data from the database

---
+++ periodic.js
@@ -0,0 +1,25 @@
+import {scheduleJob, gracefulShutdown } from 'node-schedule'
+
+import scrapLeaders from './lib/scrap_leaders.js'
+import scrapMembers from './lib/scrap_members.js'
+import { getBrowserAndPageObject } from './lib/main.js'
+
+const run = async function() {
+	const { browser, page } = await getBrowserAndPageObject()
+	await scrapLeaders(page);
+	await scrapMembers(page);
+	await browser.close()
+}
+
+scheduleJob('* * 6 * * *', async function () {
+	await run();
+})
+
+scheduleJob('* * 12 * * *', async function () {
+	await run();
+})
+
+
+process.on('SIGINT', function () {
+	gracefulShutdown().then(() => process.exit(0))
+})
